acb7c1fec3c526c8a9a1ffabf5a4cba19c80d717
GiuseppeBarresi98 <145044894+GiuseppeBarresi98@users.noreply.github.com>
2024-02-23 15:10:23 +0100

    validator on user

--- src/main/java/com/barresi/gestioneEventi/payloads/UserDTO.java
+++ src/main/java/com/barresi/gestioneEventi/payloads/UserDTO.java
@@ -1,14 +1,19 @@
 package com.barresi.gestioneEventi.payloads;
 
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class UserDTO {
+    @NotEmpty(message = "inserire username")
     private String username;
+    @NotEmpty(message = "Nome )
     private String name;
+    @NotEmpty(message = "Email obbligatoria")
     private String email;
+    @NotEmpty(message = "Password obbligatoria")
     private String password;
 }
